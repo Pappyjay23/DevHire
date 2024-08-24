61f97f66c56dc93a57067a4e0002b56c5223800b
Pappyjay23 <pjinadu02@gmail.com>
2024-08-24 13:04:44 +0100

    feat: Updated seo params

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
-    port: 3000
+    port: 3000,
+    host: '0.0.0.0'
   },
   resolve: {
     alias: {
