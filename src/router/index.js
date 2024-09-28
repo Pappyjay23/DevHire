5ae8088fddac0294104dba3d5ccf356e6ee58791
Pappyjay23 <pjinadu02@gmail.com>
2024-09-28 11:18:39 +0100

    feat: Added logic for protected routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
+const isAuthenticated = () => {
+  return !!localStorage.getItem('user')
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -56,8 +60,20 @@ const router = createRouter({
     }
   ],
   scrollBehavior() {
-    // Always scroll to top
     return { top: 0 }
+  }
+})
+
+// Global before guard
+router.beforeEach((to, from, next) => {
+  // Define the routes that do not require authentication
+  const publicRoutes = ['home', 'login', 'sign-up']
+
+  // Check if the route is public or requires authentication
+  if (publicRoutes.includes(to.name) || isAuthenticated()) {
+    next() // Allow navigation
+  } else {
+    next({ name: 'login' }) // Redirect to login if not authenticated
   }
 })
 
